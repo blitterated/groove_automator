63150ca924095bc24a67135721a54be1b038a318
blitterated <blitterated@protonmail.com>
2024-01-05 14:34:38 -0600

    Added second groove

--- groove_automator.rb
+++ groove_automator.rb
@@ -91,4 +91,49 @@ class Grooves
         "S=#{measures.sd}&" +
         "K=#{measures.kd}"
     end
+
+    def groove_2
+        groove = OpenStruct.new({
+            time_sig: "4/4",
+            subdiv: 16,
+            tempo: 80,
+            measures: 18,
+            hh: "s-x-",
+            sn: "-gg-O--g-gg-O--g-gg-O--g-gg-O---g",
+            kd: "o--o----o--o----o--o----o--o-----"
+        })
+
+        high_hat_pattern =   Pattern.new(name: "Hi-Hat", pattern: groove.hh)
+        snare_drum_pattern = Pattern.new(name: "Snare", pattern: groove.sn)
+        kick_drum_pattern =  Pattern.new(name: "Kick", pattern: groove.kd)
+
+        measures = OpenStruct.new({
+            hh: "|",
+            sd: "|",
+            kd: "|"
+        })
+
+        ts = @time_signatures[groove.time_sig]
+
+        (1..groove.measures).each do
+            (1..ts.subdivisions_per_measure(groove.subdiv)).each do
+                measures.hh += high_hat_pattern.next
+                measures.sd += snare_drum_pattern.next
+                measures.kd += kick_drum_pattern.next
+            end
+
+            measures.hh += "|"
+            measures.sd += "|"
+            measures.kd += "|"
+        end
+
+        "http://localhost:8080/?" +
+        "TimeSig=#{groove.time_sig}&" +
+        "Div=#{groove.subdiv}&" +
+        "Tempo=#{groove.tempo}&" +
+        "Measures=#{groove.measures}&" +
+        "H=#{measures.hh}&" +
+        "S=#{measures.sd}&" +
+        "K=#{measures.kd}"
+    end
 end
