8ce042ef76219ce3d6f2c8942b9a16887c8ab995
blitterated <blitterated@protonmail.com>
2024-01-26 05:42:07 -0600

    More readability and less freeze for ABCGlyphs

--- lib/gs_groove_automator/abc_glyphs.rb
+++ lib/gs_groove_automator/abc_glyphs.rb
@@ -3,52 +3,53 @@
 
 class ABCGlyphs
 
-  @@hihat = {
-    normal: "x".freeze,
-    open:   "o".freeze,
-    accent: "X".freeze
+  @@glyph_tree = {
+    hihat: {
+      normal: "x",
+      open:   "o",
+      accent: "X"
+    },
+
+    cymbal: {
+      crash: "c",
+      ride: "r",
+      bell: "b"
+    },
+
+    perc: {
+      cowbell: "m",
+      stacker: "s",
+      click: "n",
+      click_accent: "N"
+    },
+
+    snare: {
+       normal: "o",
+       accent: "O",
+       ghost: "g",
+       cross_stick: "x",
+       buzz: "b",
+       flam: "f"
+    },
+
+    pedal: {
+      kick: "o",
+      hat: "x",
+      both: "X"
+    }
   }
 
-  @@cymbal = {
-    crash: "c".freeze,
-    ride: "r".freeze,
-    bell: "b".freeze
-  }
-
-  @@perc = {
-    cowbell: "m".freeze,
-    stacker: "s".freeze,
-    click: "n".freeze,
-    click_accent: "N".freeze
-  }
-
-  @@snare = {
-     normal: "o".freeze,
-     accent: "O".freeze,
-     ghost: "g".freeze,
-     cross_stick: "x".freeze,
-     buzz: "b".freeze,
-     flam: "f".freeze
-  }
-
-  @@pedal = {
-    kick: "o".freeze,
-    hat: "x".freeze,
-    both: "X".freeze
-  }
-
-  @@glyph_types = [ :hihat, :cymbal, :perc, :snare, :pedal ]
-
-  @@glyph_types.each do |gt|
-    struct_name = "ABC#{gt.to_s.capitalize}Glyphs"
-    glyphs = self.singleton_class.class_variable_get("@@#{gt}")
-             .then do |g|
-               Struct.new(struct_name, *g.keys).new(**g).freeze
-             end
-    define_singleton_method gt, -> { glyphs }
+  @@glyph_tree.each do |glyph_type, glyphs|
+    struct_name = "ABC#{glyph_type.to_s.capitalize}Glyphs"
+    frosty_glyphs = glyphs.map { |k,v| [k, v.freeze] }.to_h
+    frosty_glyph_struct =
+      Struct.new(struct_name, *frosty_glyphs.keys)
+            .new(**frosty_glyphs).freeze
+    define_singleton_method glyph_type, -> { frosty_glyph_struct }
   end
 
   def self.types
-      @@glyph_types
+      return @@glyph_types if defined? @@glyph_types
+      @@glyph_types = @@glyph_tree.keys.dup.freeze
   end
 end
