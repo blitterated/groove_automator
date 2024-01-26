173e27433e2f1bf7fddca65ce8b4b91f0834b826
blitterated <blitterated@protonmail.com>
2024-01-26 05:12:31 -0600

    Made ABCGlyphs readable again. More metaprogramming.

--- lib/gs_groove_automator/abc_glyphs.rb
+++ lib/gs_groove_automator/abc_glyphs.rb
@@ -3,69 +3,52 @@
 
 class ABCGlyphs
 
-  @@glyph_tree = {
-    hihat: -> {
-      return @@hihat if defined? @@hihat
-      @@hihat = {
-        normal: "x".freeze,
-        open:   "o".freeze,
-        accent: "X".freeze
-      }.then do |h|
-        Struct.new("ABCHiHatGlyphs", *h.keys).new(**h)
-      end
-    },
-    cymbal: -> {
-      return @@cymbal if defined? @@cymbal
-      @@cymbal = {
-        crash: "c".freeze,
-        ride: "r".freeze,
-        bell: "b".freeze
-      }.then do |c|
-        Struct.new("ABCCymbalGlyphs", *c.keys).new(**c)
-      end
-    },
-    perc: -> {
-      return @@perc if defined? @@perc
-      @@perc = {
-        cowbell: "m".freeze,
-        stacker: "s".freeze,
-        click: "n".freeze,
-        click_accent: "N".freeze
-      }.then do |p|
-        Struct.new("ABCPercussionGlyphs", *p.keys).new(**p)
-      end
-    },
-    snare: -> {
-      return @@snare if defined? @@snare
-      @@snare = {
-         normal: "o".freeze,
-         accent: "O".freeze,
-         ghost: "g".freeze,
-         cross_stick: "x".freeze,
-         buzz: "b".freeze,
-         flam: "f".freeze
-      }.then do |s|
-        Struct.new("ABCSnareGlyphs", *s.keys).new(**s)
-      end
-    },
-    pedal: -> {
-      return @@pedal if defined? @@pedal
-      @@pedal = {
-        kick: "o".freeze,
-        hat: "x".freeze,
-        both: "X".freeze
-      }.then do |p|
-        Struct.new("ABCPedalGlyphs", *p.keys).new(**p)
-      end
-    }
-  }.freeze
+  @@hihat = {
+    normal: "x".freeze,
+    open:   "o".freeze,
+    accent: "X".freeze
+  }
 
-  @@glyph_tree.each do |method_name, method_body|
-    define_singleton_method method_name, &method_body
+  @@cymbal = {
+    crash: "c".freeze,
+    ride: "r".freeze,
+    bell: "b".freeze
+  }
+
+  @@perc = {
+    cowbell: "m".freeze,
+    stacker: "s".freeze,
+    click: "n".freeze,
+    click_accent: "N".freeze
+  }
+
+  @@snare = {
+     normal: "o".freeze,
+     accent: "O".freeze,
+     ghost: "g".freeze,
+     cross_stick: "x".freeze,
+     buzz: "b".freeze,
+     flam: "f".freeze
+  }
+
+  @@pedal = {
+    kick: "o".freeze,
+    hat: "x".freeze,
+    both: "X".freeze
+  }
+
+  @@glyph_types = [ :hihat, :cymbal, :perc, :snare, :pedal ]
+
+  @@glyph_types.each do |gt|
+    struct_name = "ABC#{gt.to_s.capitalize}Glyphs"
+    glyphs = self.singleton_class.class_variable_get("@@#{gt}")
+             .then do |g|
+               Struct.new(struct_name, *g.keys).new(**g).freeze
+             end
+    define_singleton_method gt, -> { glyphs }
   end
 
   def self.types
-      return @@types if defined? @@types
-      @@types = @@glyph_tree.keys.dup.freeze
+      @@glyph_types
   end
 end
